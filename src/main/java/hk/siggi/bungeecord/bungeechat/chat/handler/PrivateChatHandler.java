a356ee6ac9416aad137d24f363ba156fdc4b0294
Ash <31135263+Asheiou@users.noreply.github.com>
2022-02-11 23:41:40 +0000

    Colour the PM channel subtitle back to &e

--- src/main/java/hk/siggi/bungeecord/bungeechat/chat/handler/PrivateChatHandler.java
+++ src/main/java/hk/siggi/bungeecord/bungeechat/chat/handler/PrivateChatHandler.java
@@ -67,7 +67,7 @@ public class PrivateChatHandler implements ChatHandler {
 	@Override
 	public List<BaseComponent> getDisplay() {
 		List<BaseComponent> result = new ArrayList<>();
-		result.addAll(processChat(null, "&7PM To "));
+		result.addAll(processChat(null, "&ePM To "));
 		result.addAll(usernameComponent);
 		return result;
 	}
