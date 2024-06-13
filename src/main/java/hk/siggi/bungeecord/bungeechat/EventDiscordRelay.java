f613c0d35cfe79dc43ce02277ef6d67e417789bd
Siggi <7286178+Siggi88@users.noreply.github.com>
2024-06-13 00:54:56 +0000

    Add a bit more detail to hack-watch messages, and make code a bit cleaner

--- src/main/java/hk/siggi/bungeecord/bungeechat/EventDiscordRelay.java
+++ src/main/java/hk/siggi/bungeecord/bungeechat/EventDiscordRelay.java
@@ -6,10 +6,25 @@ import hk.siggi.bungeecord.bungeechat.util.DiscordBotAPI;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class EventDiscordRelay implements Listener {
 	private final BungeeChat plugin;
 	public EventDiscordRelay(BungeeChat plugin) {
 		this.plugin = plugin;
+	}
+
+	private static final TimeZone gmt = TimeZone.getTimeZone("GMT");
+	private static String timeToString(long time) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd-HH:mm:ss");
+		sdf.setTimeZone(gmt);
+		return sdf.format(new Date(time)) + "-GMT";
+	}
+
+	private static String doubleToString(double value) {
+		return Double.toString(Math.round(value * 100.0) / 100.0);
 	}
 
 	@EventHandler
@@ -20,12 +35,23 @@ public class EventDiscordRelay implements Listener {
 		if (!event.getOre().equals("diamond") && !event.getOre().equals("emerald")) {
 			return;
 		}
-		DiscordBotAPI.sendMessage("hack-watch", event.getPlayer().getName() + " found " + event.getCount() + "x " + event.getOre() + "\nServer: " + event.getServer().getName(), false);
+		DiscordBotAPI.sendMessage("hack-watch",
+				event.getPlayer().getName()
+						+ " found " + event.getCount() + "x " + event.getOre()
+						+ "\nServer: " + event.getServer().getName()
+						+ "\nTime: " + timeToString(System.currentTimeMillis()),
+				false);
 	}
 
 	@EventHandler
 	public void playerSpeeding(PlayerSpeedingEvent event) {
 		if (event.getGameMode() == 1 || event.getGameMode() == 3) return; // ignore speeding for creative mode and spectator mode
-		DiscordBotAPI.sendMessage("hack-watch", event.getPlayer().getName() + " is speeding! Speed: " + event.getSpeed3D() + " bps, Vertical speed: " + event.getSpeedY(), false);
+		DiscordBotAPI.sendMessage("hack-watch",
+				event.getPlayer().getName() + " is speeding!"
+						+ "\nSpeed: " + doubleToString(event.getSpeed3D()) + " bps"
+						+ "\nVertical speed: " + doubleToString(event.getSpeedY()) + " bps"
+						+ "\nServer: " + event.getPlayer().getServer().getInfo().getName()
+						+ "\nTime: " + timeToString(System.currentTimeMillis()),
+				false);
 	}
 }
